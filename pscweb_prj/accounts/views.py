23c2037eecf002ec139ab05e75b1c15c96573af4
satamame <satax@nifty.com>
2018-06-11 01:00:28 +0900

    Adding email address field to sign-up form.

--- pscweb_prj/accounts/views.py
+++ pscweb_prj/accounts/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import SignUpForm
 
 @login_required
 def profile(request):
@@ -12,6 +13,6 @@ def profile(request):
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
