84eda96a2c5708016b58ecd539efb8adb29bc34e
satamame <satax@nifty.com>
2018-08-19 15:39:35 +0900

    Model 'Production' is added to 'gs_schdl' app.

--- pscweb_prj/gs_schdl/models.py
+++ pscweb_prj/gs_schdl/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Production(models.Model):
+    title = models.CharField('Title of production', max_length=255)
+    gs_id = models.CharField('ID of Google spreadsheet', max_length=255)
+
+    def __str__( self):
+        return self. title
