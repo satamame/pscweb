8bbc253fe29a0dfded9247e36adfad15520b4239
satamame <satax@nifty.com>
2018-08-23 01:04:00 +0900

    Adding Member and Team models.

--- pscweb_prj/gs_schdl/admin.py
+++ pscweb_prj/gs_schdl/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Production
+from .models import Production, Member, Team
 
-# Register your models here.
 admin.site.register(Production)
+admin.site.register(Member)
+admin.site.register(Team)
--- pscweb_prj/gs_schdl/models.py
+++ pscweb_prj/gs_schdl/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
-# Create your models here.
 class Production(models.Model):
     title = models.CharField('Title of production', max_length=255)
     gs_id = models.CharField('ID of Google spreadsheet', max_length=255)
 
-    def __str__( self):
-        return self. title
+    def __str__(self):
+        return self.title
+
+
+class Member(models.Model):
+    prod_id = models.ForeignKey(Production, on_delete=models.CASCADE)
+    name = models.CharField('Name of member', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    prod_id = models.ForeignKey(Production, on_delete=models.CASCADE)
+    name = models.CharField('Name of team', max_length=255)
+    members = models.ManyToManyField(Member)
+
+    def __str__(self):
+        return self.name
+
