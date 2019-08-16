bbf3318b8fdbcb83d5fbf9ccaca2215b6f71e654
satamame <satax@nifty.com>
2019-08-17 01:19:30 +0900

    Django is updated, scripts label view is added.

--- scripts/urls.py
+++ scripts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.scripts, name='sc_index'), # Index of scripts
     # ex: /scripts/convert/
     path('convert/', views.convert, name='sc_convert'), # Script Converter
+    # ex: /scripts/label/
+    path('label/', views.label, name='sc_label'), # Labeling script lines
 ]
--- scripts/views.py
+++ scripts/views.py
@@ -8,3 +8,16 @@ def scripts(request):
 def convert(request):
     context = {}
     return render(request, 'scripts/convert.html', context)
+
+
+def label(request):
+    script = b''
+    uploaded = request.FILES['file']
+    for chunk in uploaded.chunks():
+        script += chunk
+    script = script.decode('utf-8')
+    
+    lines = script.split('\n')
+    
+    context = {'lines' : lines}
+    return render(request, 'scripts/label.html', context)
